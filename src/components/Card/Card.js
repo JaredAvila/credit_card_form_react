bf99b7d479116f1ccab62c6efc56de56bdc9a50f
Jared Avila <jared.c.avila@gmail.com>
2020-01-02 22:20:08 -0800

    setting up project

---
+++ src/components/Card/Card.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div>
+      <p>Card Component</p>
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/containers/Landing/Landing.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "../../components/Card/Card";
+import Form from "../../components/Form/Form";
+
+function Landing() {
+  return (
+    <div>
+      <Card />
+      <Form />
+    </div>
+  );
+}
+
+export default Landing;
